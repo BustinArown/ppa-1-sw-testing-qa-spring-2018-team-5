058161a81d959ec4df073b7ecbf66fbdeb2c2f53
Lucas Kramer <lk356@msstate.edu>
2018-02-13 16:21:00 -0600

    Refactor is_valid_email

--- email_verifier.py
+++ email_verifier.py
@@ -3,6 +3,4 @@ import re
 
 def is_valid_email(address):
     regex = re.compile('[\w]+@')
-    if regex.match(address) is None:
-        return False
-    return True
+    return regex.match(address) is not None
