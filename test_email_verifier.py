66e3851d5d2314a1e02c1b0c153a880a3ff3fd55
Lucas Kramer <lk356@msstate.edu>
2018-02-14 15:01:52 -0600

    Add allowed symbols test

--- test_email_verifier.py
+++ test_email_verifier.py
@@ -18,5 +18,9 @@ class TestEmailVerifier(unittest.TestCase):
 
     def test_domain_format(self):
         self.assertFalse(is_valid_email('testemail@domain'))
+
+    def test_allow_symbols(self):
+        self.assertTrue(is_valid_email('testemail!$%*+-=?^_{|}~@domain.com'))
+
 if __name__ == '__main__':
     unittest.main()
