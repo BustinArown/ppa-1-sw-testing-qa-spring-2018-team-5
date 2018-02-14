ca3148bb6dea42dcf749b10435666da1f64206c0
Lucas Kramer <lk356@msstate.edu>
2018-02-13 21:47:26 -0600

    Make domain format test pass

--- email_verifier.py
+++ email_verifier.py
@@ -2,5 +2,5 @@ import re
 
 
 def is_valid_email(address):
-    regex = re.compile('[a-zA-Z][[.]?\w+]*@\w')
+    regex = re.compile('[a-zA-Z][[.]?\w+]*[@]\w+[.]\w+')
     return regex.match(address) is not None
