4b81db98f29feaf4b13a59dc3f6265fd89826ef6
teriannaj <tdj190@msstate.edu>
2018-02-14 11:46:40 -0600

    Add files via upload

---
+++ test_bmi.py
@@ -0,0 +1,40 @@
+import unittest
+import BMI
+
+class Test_bmi(unittest.TestCase):
+
+    def test_main(self):
+        self.assertRaises(ValueError, BMI.main())
+
+    def test_valid_input(self):
+        self.assertRaises(ValueError, BMI.bmi(5, 3, 125))
+
+    def test_height_conversion(self):
+        self.assertEqual(BMI.convert(5, 3), 63)
+
+    def test_determine(self):
+        self.assertEqual(BMI.determine(22), "Normal weight")
+
+    def test_invalid_inputF(self):
+        self.assertRaises(ValueError, BMI.bmi('lll', 3, 125))
+
+    def test_invalid_inputI(self):
+        self.assertRaises(ValueError, BMI.bmi(5, 'blah', 125))
+
+    def test_invalid_inputW(self):
+        self.assertRaises(ValueError, BMI.bmi(5, 3, 'kkk'))
+
+
+
+
+
+
+
+
+
+
+if __name__ == '__main__':
+    unittest.main()
+
+
+TestRunner.run()
