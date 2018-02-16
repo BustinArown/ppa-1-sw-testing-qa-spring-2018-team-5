d40424c466f72c2a982b0c98f89ea68ed020e8e8
OluwaDemilade <35817160+OluwaDemilade@users.noreply.github.com>
2018-02-15 19:55:30 -0600

    Add files via upload

--- shortest_distance.py
+++ shortest_distance.py
@@ -2,21 +2,10 @@
 #this function finds the distance between two points (x1,y1) and (x2,y2)
 import math
 
-def distance (x1,y1,x2,y2):
-    #return 0
-    if x2 >= x1 and y2 >= y1:
-        return (math.sqrt(((x2-x1)**2) + ((y2-y1)**2)))
-    else:
-        raise ValueError('x2 and y2 must be greater than x1 and y1 respectively')
-    #round(math.sqrt(((x2-x1)**2) + ((y2-y1)**2)),2)
 
+
+def distance (x1,y1,x2,y2):
+    return round((math.sqrt(((x2-x1)**2) + ((y2-y1)**2))),4)
 
 def floatValues(x1,y1,x2,y2):
     return float(x1),float(y1),float(x2),float(y2)
-    #return float(math.sqrt(((x2-x1)**2) + ((y2-y1)**2)))
-
-
-
-print distance(2,4,5,8)
-#print not_zero (1,2,5,0)
-#print floatValues(0,0,0,0)
--- test_shortest_distance.py
+++ test_shortest_distance.py
@@ -4,19 +4,17 @@ import shortest_distance
 
 class Test_shortest_distance(unittest.TestCase):
 
-    def test_distance(self):
 
+    def test_distance(self):
         self.assertEqual(shortest_distance.distance(1,1,1,1), 0.0)
-        self.assertTrue((shortest_distance.distance(2,3,1,2)),round(1.4142135623730951,4))
-        self.assertFalse(shortest_distance.distance())
+        self.assertTrue((shortest_distance.distance(2,3,1,2)),1.4142135623730951)
+        self.assertNotEqual(shortest_distance.distance(2,3,4,5),2.345566)
 
     def test_floatvalues(self):
-        #self.assertTrue(shortest_distance.floatValues(0,0,0,0))
         self.assertTrue(shortest_distance.floatValues(1,1,1,1))
         self.assertTrue(shortest_distance.floatValues(2,4,5,8))
         self.assertTrue(shortest_distance.floatValues(-2,-1,-3,-6))
         self.assertTrue(shortest_distance.floatValues(-2,1,-3,6))
-
 
 
 if __name__=='__main__':
