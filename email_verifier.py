a425a3454fcf5bb80534780604e372e94bde2d5a
Lucas Kramer <lk356@msstate.edu>
2018-02-16 16:59:23 -0600

    Make symbol test pass

--- email_verifier.py
+++ email_verifier.py
@@ -2,5 +2,8 @@ import re
 
 
 def is_valid_email(address):
-    regex = re.compile('[a-zA-Z][[.]?\w+]*[@]\w+[.]\w+')
+    double_period_ex = re.compile('[.][.]')
+    if double_period_ex.search(address) is not None:
+        return False
+    regex = re.compile('[a-z][a-z0-9!$%^*+-={|}._?~]*[@][a-z0-9!$%^*+-={|}_?~]+[.][a-z0-9!$%^*+-={|}_?~]+', re.IGNORECASE)
     return regex.match(address) is not None
