42e9b11976366880ab2b8d5c425cee3d7c62bac6
Lucas Kramer <lk356@msstate.edu>
2018-02-16 18:20:52 -0600

    Refactor is_valid_email

--- email_verifier.py
+++ email_verifier.py
@@ -3,7 +3,5 @@ import re
 
 def is_valid_email(address):
     double_period_ex = re.compile('[.][.]')
-    if double_period_ex.search(address) is not None:
-        return False
     regex = re.compile('[a-z][a-z0-9!$%^*+-={|}._?~]*[@][a-z0-9!$%^*+-={|}_?~]+[.][a-z0-9!$%^*+-={|}_?~]+', re.IGNORECASE)
-    return regex.match(address) is not None
+    return double_period_ex.search(address) is None and regex.match(address) is not None
