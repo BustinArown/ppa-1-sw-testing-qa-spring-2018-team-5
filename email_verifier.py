0472a715dfe4cab9e8b198734e2da401b11a3209
Lucas Kramer <lk356@msstate.edu>
2018-02-17 14:37:44 -0600

    Make non-string input test pass

--- email_verifier.py
+++ email_verifier.py
@@ -1,7 +1,6 @@
-import re
-
 
 def is_valid_email(address):
-    double_period_ex = re.compile('[.][.]')
-    regex = re.compile('[a-z][a-z0-9!$%^*+-={|}._?~]*[@][a-z0-9!$%^*+-={|}_?~]+[.][a-z0-9!$%^*+-={|}_?~]+', re.IGNORECASE)
-    return double_period_ex.search(address) is None and regex.match(address) is not None
+    import re
+    return re.compile('[.][.]').search(str(address)) is None and \
+            re.compile('[a-z][a-z0-9!$%^*+-={|}._?~]*[@][a-z0-9!$%^*+-={|}_?~]+[.][a-z0-9!$%^*+-={|}_?~]+', re.IGNORECASE).match(str(address)) is not None
+    
