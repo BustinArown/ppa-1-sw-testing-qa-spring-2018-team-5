5d0e2c2ba38d9002a37dca4dcd21450a33669840
Lucas Kramer <lk356@msstate.edu>
2018-02-17 14:40:47 -0600

    Refactor for higher coverage

--- email_verifier.py
+++ email_verifier.py
@@ -1,6 +1,5 @@
+import re
 
 def is_valid_email(address):
-    import re
     return re.compile('[.][.]').search(str(address)) is None and \
             re.compile('[a-z][a-z0-9!$%^*+-={|}._?~]*[@][a-z0-9!$%^*+-={|}_?~]+[.][a-z0-9!$%^*+-={|}_?~]+', re.IGNORECASE).match(str(address)) is not None
-    
