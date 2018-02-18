56a899b3cf895dda6a2e36c0ae37c77b2beed997
Lucas Kramer <lk356@msstate.edu>
2018-02-18 15:33:32 -0600

    Refactor code and eliminate redundant test

--- test_email_verifier.py
+++ test_email_verifier.py
@@ -4,20 +4,23 @@ from email_verifier import *
 
 class TestEmailVerifier(unittest.TestCase):
 
-    def test_first_character_non_numeric(self):
-        self.assertFalse(is_valid_email('1asdf@domain'))
-
-    def test_double_periods(self):
-        self.assertFalse(is_valid_email('test..email@domain.com'))
-
-    def test_leading_characters(self):
-        self.assertFalse(is_valid_email('@domain.com'))
+    def test_empty_address(self):
+        self.assertFalse(is_valid_email(''))
 
     def test_contains_at(self):
         self.assertTrue(is_valid_email('testemail@domain.com'))
 
+    def test_leading_characters(self):
+        self.assertFalse(is_valid_email('@domain.com'))
+
+    def test_first_character_non_numeric(self):
+        self.assertFalse(is_valid_email('1asdf@domain'))
+
     def test_domain_format(self):
         self.assertFalse(is_valid_email('testemail@domain'))
+
+    def test_double_periods(self):
+        self.assertFalse(is_valid_email('test..email@domain.com'))
 
     def test_allow_symbols(self):
         self.assertTrue(is_valid_email('testemail!$%*+-=?^_{|}~@domain.com'))
@@ -25,11 +28,6 @@ class TestEmailVerifier(unittest.TestCase):
     def test_disallowed_symbols(self):
         self.assertFalse(is_valid_email('testemail"\'`'))
 
-    def test_empty_address(self):
-        self.assertFalse(is_valid_email(''))
-
-    def test_input_is_string(self):
-        self.assertFalse(is_valid_email(int(5)))
 
 if __name__ == '__main__':
     unittest.main()
