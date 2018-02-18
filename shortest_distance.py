6cfec246eaa2378436078e6f4495cf73470d44ab
OluwaDemilade <35817160+OluwaDemilade@users.noreply.github.com>
2018-02-18 02:26:55 -0600

    Add files via upload

--- shortest_distance.py
+++ shortest_distance.py
@@ -2,10 +2,57 @@
 #this function finds the distance between two points (x1,y1) and (x2,y2)
 import math
 
+#def distance(x1,y1,x2,y2):
+#    print 'x1=',x1, "\n" 'y1=',y1, "\n" 'x2=',x2, "\n" 'y2=',y2
+#    if x2>x1 and y2>y1:
+#        print 'x2-x1=',(x2 - x1)  ,"\n",'y2-y1=',(y2-y1)
+#        print "(x2-x1)**2=",((x2-x1)**2),"\n",'(y2-y1)**2=', ((y2-y1)**2)
+##        print 'square root of ((x2-x1)**2) + ((y2-y1)**2)=', round((math.sqrt(((x2-x1)**2) + ((y2-y1)**2))),4)
+#    else:
+#        print "x2 and y2 must be greater than x1 and y1"
 
+
+def subtract (x1,y1,x2,y2):
+    if x2>x1 and y2>y1:
+        #return "SUBTRACT (x2-x1), (y2-y1)"
+        return (x2-x1), (y2-y1)
+    else:
+        raise ValueError('x2 and y2 must be greater than x1 and y1 respectively')
+
+
+def square (x1,y1,x2,y2):
+
+        # "SQUARE ((y2-y1)**2), ((y2-y1)**2)"
+        return (x2-x1)**2, (y2-y1)**2
+
+
+def add(x1,y1,x2,y2):
+
+        #print "ADD (((x2-x1)**2) + ((y2-y1)**2))"
+        return ((x2-x1)**2) + ((y2-y1)**2)
 
 def distance (x1,y1,x2,y2):
-    return round((math.sqrt(((x2-x1)**2) + ((y2-y1)**2))),4)
 
-def floatValues(x1,y1,x2,y2):
-    return float(x1),float(y1),float(x2),float(y2)
+        #print "DISTANCE= (squareroot of(((x2-x1)**2) + ((y2-y1)**2)))"
+        return float(round((math.sqrt(((x2-x1)**2) + ((y2-y1)**2))),4))
+
+def enterValues():
+
+    x1=int(input("x1="))
+    y1=int(input("y1="))
+    x2=int(input("x2="))
+    y2=int(input("y2="))
+
+
+    if x2>x1 and y2>y1:
+        print subtract(x1,y1,x2,y2)
+        print square(x1,y1,x2,y2)
+        print add(x1,y1,x2,y2)
+        print distance(x1,y1,x2,y2)
+    else:
+        raise ValueError('x2 and y2 must be greater than x1 and y1 respectively')
+
+
+def main():
+    enterValues()
+if __name__ == '__main__':main()
--- test_shortest_distance.py
+++ test_shortest_distance.py
@@ -5,16 +5,23 @@ import shortest_distance
 class Test_shortest_distance(unittest.TestCase):
 
 
-    def test_distance(self):
-        self.assertEqual(shortest_distance.distance(1,1,1,1), 0.0)
-        self.assertTrue((shortest_distance.distance(2,3,1,2)),1.4142135623730951)
-        self.assertNotEqual(shortest_distance.distance(2,3,4,5),2.345566)
+    def test_greater(self):
+        self.assertRaises(AssertionError, shortest_distance.enterValues())
 
-    def test_floatvalues(self):
-        self.assertTrue(shortest_distance.floatValues(1,1,1,1))
-        self.assertTrue(shortest_distance.floatValues(2,4,5,8))
-        self.assertTrue(shortest_distance.floatValues(-2,-1,-3,-6))
-        self.assertTrue(shortest_distance.floatValues(-2,1,-3,6))
+    def test_subtract(self):
+        self.assertEqual(shortest_distance.subtract(2,3,4,6), (2,3))
+        self.assertNotEqual(shortest_distance.subtract(2,1,4,5),(3,2))
+
+    def test_square(self):
+        self.assertEqual(shortest_distance.square(3,4,5,6), (4,4))
+
+    def test_add(self):
+        self.assertEqual(shortest_distance.add(2,3,4,4),(5))
+
+    def test_distance(self):
+        self.assertEqual(shortest_distance.distance(4,3,6,5), 2.8284)
+        self.assertEqual((shortest_distance.distance(-1,-1,0,0)),1.4142)
+
 
 
 if __name__=='__main__':
