ce7993435749806cc199a3640a05760f18e8bce6
OluwaDemilade <35817160+OluwaDemilade@users.noreply.github.com>
2018-02-18 02:44:10 -0600

    Add files via upload

--- shortest_distance.py
+++ shortest_distance.py
@@ -3,19 +3,19 @@
 import math
 
 
-def subtract (x1,y1,x2,y2):
+def subtract (x1,y1,x2,y2):              #subtracts points (x2-x1) and (y2-y1)
         return (x2-x1), (y2-y1)
 
 
-def square (x1,y1,x2,y2):
+def square (x1,y1,x2,y2):               #squares both outputs after subtracting
         return (x2-x1)**2, (y2-y1)**2
 
 
 def add(x1,y1,x2,y2):
-        return ((x2-x1)**2) + ((y2-y1)**2)
+        return ((x2-x1)**2) + ((y2-y1)**2)  #adds the outputs together after squaring
 
 
-def distance (x1,y1,x2,y2):
+def distance (x1,y1,x2,y2):                 #find squareroot of the output after adding(the shortest distance of the given points)
         return float(round((math.sqrt(((x2-x1)**2) + ((y2-y1)**2))),4))
 
 
--- test_shortest_distance.py
+++ test_shortest_distance.py
@@ -5,20 +5,20 @@ import shortest_distance
 class Test_shortest_distance(unittest.TestCase):
 
 
-    def test_greater(self):
+    def test_greater(self):             #tests that x2 is greater than x1 and y2 is greater than y1
         self.assertRaises(AssertionError, shortest_distance.enterValues())
 
-    def test_subtract(self):
+    def test_subtract(self):            #tests that the subtraction operation is correct
         self.assertEqual(shortest_distance.subtract(2,3,4,6), (2,3))
         self.assertNotEqual(shortest_distance.subtract(2,1,4,5),(3,2))
 
-    def test_square(self):
+    def test_square(self):              #tests that the square operation is correct
         self.assertEqual(shortest_distance.square(3,4,5,6), (4,4))
 
-    def test_add(self):
+    def test_add(self):                 #tests that the addition operation is correct
         self.assertEqual(shortest_distance.add(2,3,4,4),(5))
 
-    def test_distance(self):
+    def test_distance(self):            #tests that the distance calculation is correct
         self.assertEqual(shortest_distance.distance(4,3,6,5), 2.8284)
         self.assertEqual((shortest_distance.distance(-1,-1,0,0)),1.4142)
 
