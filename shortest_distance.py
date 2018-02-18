96dd9dc67dba2c3a37941ea96386764995c89d47
Lucas Kramer <lk356@msstate.edu>
2018-02-18 15:53:48 -0600

    Refactor, eliminate unneeded tests and functions
    Condenses the distance formula into one function since if basic math
    does not work, the computer is garbage. Code follow PEP8 style

--- shortest_distance.py
+++ shortest_distance.py
@@ -1,39 +1,5 @@
-
-#this function finds the distance between two points (x1,y1) and (x2,y2)
 import math
 
 
-def subtract (x1,y1,x2,y2):              #subtracts points (x2-x1) and (y2-y1)
-        return (x2-x1), (y2-y1)
-
-
-def square (x1,y1,x2,y2):               #squares both outputs after subtracting
-        return (x2-x1)**2, (y2-y1)**2
-
-
-def add(x1,y1,x2,y2):
-        return ((x2-x1)**2) + ((y2-y1)**2)  #adds the outputs together after squaring
-
-
-def distance (x1,y1,x2,y2):                 #find squareroot of the output after adding(the shortest distance of the given points)
-        return float(round((math.sqrt(((x2-x1)**2) + ((y2-y1)**2))),4))
-
-
-def enterValues():
-
-    x1=int(input("x1="))
-    y1=int(input("y1="))
-    x2=int(input("x2="))
-    y2=int(input("y2="))
-
-    if x2>x1 and y2>y1:
-        print subtract(x1,y1,x2,y2)
-        print square(x1,y1,x2,y2)
-        print add(x1,y1,x2,y2)
-        print distance(x1,y1,x2,y2)
-    else:
-        raise ValueError('x2 and y2 must be greater than x1 and y1 respectively')
-
-def main():
-    enterValues()
-if __name__ == '__main__':main()
+def distance(x1, y1, x2, y2):  # this function finds the distance between two points (x1,y1) and (x2,y2)
+    return round(math.sqrt((x2 - x1) ** 2 + (y2 - y1) ** 2), 4)
--- test_shortest_distance.py
+++ test_shortest_distance.py
@@ -1,28 +1,13 @@
 import unittest
-
-import shortest_distance
-
-class Test_shortest_distance(unittest.TestCase):
+from shortest_distance import *
 
 
-    def test_greater(self):             #tests that x2 is greater than x1 and y2 is greater than y1
-        self.assertRaises(AssertionError, shortest_distance.enterValues())
+class TestShortestDistance(unittest.TestCase):
 
-    def test_subtract(self):            #tests that the subtraction operation is correct
-        self.assertEqual(shortest_distance.subtract(2,3,4,6), (2,3))
-        self.assertNotEqual(shortest_distance.subtract(2,1,4,5),(3,2))
-
-    def test_square(self):              #tests that the square operation is correct
-        self.assertEqual(shortest_distance.square(3,4,5,6), (4,4))
-
-    def test_add(self):                 #tests that the addition operation is correct
-        self.assertEqual(shortest_distance.add(2,3,4,4),(5))
-
-    def test_distance(self):            #tests that the distance calculation is correct
-        self.assertEqual(shortest_distance.distance(4,3,6,5), 2.8284)
-        self.assertEqual((shortest_distance.distance(-1,-1,0,0)),1.4142)
+    def test_distance(self):  # tests that the distance calculation is correct
+        self.assertEqual(distance(4, 3, 6, 5), 2.8284)
+        self.assertEqual((distance(-1, -1, 0, 0)), 1.4142)
 
 
-
-if __name__=='__main__':
+if __name__ == '__main__':
     unittest.main()
