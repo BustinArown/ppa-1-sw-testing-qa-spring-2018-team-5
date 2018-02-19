a80c5248f74b8d508b7d12a9b936662155036420
Lucas Kramer <kramer.lucas5@gmail.com>
2018-02-18 23:43:29 -0600

    Eliminate unneeded test case

--- retirement.py
+++ retirement.py
@@ -2,7 +2,7 @@ import math
 
 
 def retirement(age, salary, percentage_saved, desired_amount):
-    amount_saved_per_year = salary * (percentage_saved + 35) / 100
+    amount_saved_per_year = salary * percentage_saved / 100 * 1.35
     years_to_achieve = math.ceil(desired_amount / amount_saved_per_year)
     if years_to_achieve + age >= 100:
         return -1
--- test_retirement.py
+++ test_retirement.py
@@ -4,14 +4,11 @@ from retirement import *
 
 class TestRetirement(unittest.TestCase):
 
-    def test_age_hundred_plus_fail(self):
-        self.assertEqual(retirement(100, 10000, 20, 10000), -1)
-
     def test_goal_achieved(self):
         self.assertTrue(retirement(20, 100000, 35, 1000000) > 0)
 
     def test_goal_failed(self):
-        self.assertEqual(retirement(20, 10000, 0, 100000000), -1)
+        self.assertEqual(retirement(20, 10000, 1, 100000000), -1)
 
 
 if __name__ == '__main__':
