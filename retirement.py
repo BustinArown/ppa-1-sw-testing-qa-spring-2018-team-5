f30545ce8e0b34a2b47151407483b74d77b4db3d
Lucas Kramer <kramer.lucas5@gmail.com>
2018-02-18 20:53:45 -0600

    Complete retirement function with tests

--- retirement.py
+++ retirement.py
@@ -1,30 +1,9 @@
-#
-# Retirement- Input user's current age, annual salary, percentage saved (employer matches 35% of
-# savings). Input desired retirement savings goal. Output what age savings goal will be met. You can
-# assume death at 100 years (therefore, indicate if the savings goal is not met).
-def main():
-    print("Welcome to the Retirement Savings Calculator")
-    curr_age = int(input("How old are you currently? "))
-    salary = int(input("What is your annual salary? "))
-    saved = int(input("What percentage have you saved? "))
-    desired = int(input("What is your desired retirement savings? "))
-
-    inc_age = curr_age
-    curr_saved = (salary * (saved / 100))
-    employer = (curr_saved * (35 / 100))
-    ##following line for testing only
-    return curr_saved
-    while inc_age <= 100:
-        if inc_age == 100:
-            print("Sorry, you missed your goal.")
-            break
-        if curr_saved >= desired:
-            print("You will meet your savings goal at", inc_age)
-            break
-        else:
-            curr_saved += employer
-            inc_age += 1
+import math
 
 
-if __name__ == '__main__':
-    main()
+def retirement(age, salary, percentage_saved, desired_amount):
+    amount_saved_per_year = salary * (percentage_saved + 35) / 100
+    years_to_achieve = math.ceil(desired_amount / amount_saved_per_year)
+    if years_to_achieve + age >= 100:
+        return -1
+    return age + years_to_achieve
--- test_retirement.py
+++ test_retirement.py
@@ -1,14 +1,18 @@
 import unittest
-import main
+from retirement import *
 
 
 class TestRetirement(unittest.TestCase):
 
-    def test_curr_saved(self):
-        self.assertEqual(main.main(10000, 10), 1000)
+    def test_age_hundred_plus_fail(self):
+        self.assertEqual(retirement(100, 10000, 20, 10000), -1)
+
+    def test_goal_achieved(self):
+        self.assertTrue(retirement(20, 100000, 35, 1000000) > 0)
+
+    def test_goal_failed(self):
+        self.assertEqual(retirement(20, 10000, 0, 100000000), -1)
 
 
 if __name__ == '__main__':
     unittest.main()
-
-TestRunner.run()
