99c9d95e5200cd96c8c4d38670ac244f70393cd5
Bdrice1 <31971237+Bdrice1@users.noreply.github.com>
2018-04-28 01:03:34 -0500

    Add files via upload

---
+++ deployment.py
@@ -0,0 +1,11 @@
+import urllib2
+import logging
+
+HOST='http://ppa1team5.appspot.com'
+
+
+response = urllib2.urlopen
+#("{}/get_author/ulysses".format(HOST))
+#html = response.read()
+#assert(html == "Team5")
+
