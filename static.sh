5e8f9c94cc50584907f6efafc24d75cfefab98f0
Kenna <KennaB@users.noreply.github.com>
2018-04-29 16:38:37 -0500

    Update static.sh
    disable the "noise" and display only warnings, errors, and faults.

--- static.sh
+++ static.sh
@@ -2,6 +2,6 @@ for  item in *.py
 do
         if [ -f $item ]
         then
-                 pylint $item 
+                 pylint --disable=R --disable=W0141 --disable=C0103 --disable=C -rn $item 
         fi
 done
