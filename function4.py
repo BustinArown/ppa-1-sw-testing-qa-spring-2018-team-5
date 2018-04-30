d593bf0d06372ba516a797805b6cb58335baf4ee
Kenna <kbb269@msstate.edu>
2018-04-30 15:32:40 -0500

    Uodated python

--- function4.py
+++ function4.py
@@ -11,10 +11,14 @@ input_data=cgi.FieldStorage()
 
 
 
-def email_verifier(address):
-    return re.compile("[.][.]").search(address) is None and \
+def email_verifier(address): 
+    check = re.compile("[.][.]").search(address) is None and \
            re.compile("[a-z][a-z0-9!$%^*+-={|}._?~]*[@][a-z0-9!$%^*+-={|}_?~]+[.][a-z0-9!$%^*+-={|}_?~]+",
                       re.IGNORECASE).match(address) is not None
+    if (check):
+        print ("It is a valid address")
+    else:
+        print ("It is not a valid address")	
 
 def main():  
     print ("Content-type: text/html")
@@ -22,13 +26,15 @@ def main():
     print ("<html><head>")
     print ("</head>")
     print ("<body>")
-    
-    address = input_data["address"].value
-    if (email_verifier(address)):
-        print (address, "is a valid email.")
-    else:
-        print (address, "is an invalid email.")
-   
+    try:    
+        address = input_data["address"].value
+        if (address == ""):
+            raise ValueError
+    except ValueError:
+        print ("Wrong Input") 
+        return    
+    email_verifier(address)
+      
     print ("</body></html>")
     	
     
